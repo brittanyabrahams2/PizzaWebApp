12be791ff131ff90d4c3f384a505d6547a11e3d4
brittanyabrahams2 <43973188+brittanyabrahams2@users.noreply.github.com>
2019-07-16 08:55:23 -0400

    Customer commit #1

---
+++ Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Entities
+{
+    public class Customer
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
+        public int PhoneNo { get; set; }
+
+    }
+}
