e14dbefd83f012ab575a2cd76ebb4c0bf1320c54
brittanyabrahams2 <43973188+brittanyabrahams2@users.noreply.github.com>
2019-07-16 09:01:31 -0400

    Product commit 1

---
+++ Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Entities
+{
+    public class Product
+    {
+        public string productDescription { get; set; }
+        public int productCode { get; set; }
+        public string productName { get; set; }
+    }
+}
