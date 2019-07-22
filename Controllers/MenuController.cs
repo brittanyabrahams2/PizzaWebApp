9bb8782dc0c81910efa33be3ac67b38e2c95024e
brittanyabrahams2 <babrahams@albany.edu>
2019-07-21 20:53:53 -0400

    initial commit

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Menu()
+        {
+            return View();
+
+        }
+
+        public IActionResult Pizza()
+        {
+            return View();
+
+        }
+    }
+}
