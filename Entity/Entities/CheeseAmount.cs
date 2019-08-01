c45c0214e6d32c0798ee552088cce481d9efe5ef
ZDragon1996 <z841063918@gmail.com>
2019-08-01 13:38:37 -0400

    final

---
+++ Entity/Entities/CheeseAmount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities
+{
+    public class CheeseAmount
+    {
+        [Key]
+        public int id { get; set; }
+        public string Amount { get; set; }
+        public double Price { get; set; }
+
+    }
+}
